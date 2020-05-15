a6d224042077b959de169279aae1be99d6fc6b66
toastyce <toastyce@gmail.com>
2020-05-15 15:49:58 -0700

    ping command updated

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
       .setTitle("Ping Pong!")
       .setColor(client.config.colour)
       .setTimestamp()
-      .addField("API Latency", `${Math.round(message.client.ping)}ms`, true)
+      .addField("API Latency", `${Math.round(message.client.ws.ping)}ms`, true)
       .setImage('http://trailsinthedatabase.com/itp/1/pc/H_KAO031.png')
     message.channel.send({ embed })
     // command ends here
--- events/message.js
+++ events/message.js
@@ -1,11 +1,9 @@
 module.exports = (client, message) => {
-    // Ignore all bots
+    // filter messages for commands
     if (message.author.bot) return;
-  
-    // Ignore messages not starting with the prefix (in config.json)
     if (message.content.indexOf(client.config.prefix) !== 0) return;
   
-    // Our standard argument/command name definition.
+    // command prefix and formatting
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     client.log.console(`${message.author.tag} used the '${command}' command`)
@@ -13,7 +11,6 @@ module.exports = (client, message) => {
     // Grab the command data from the client.commands Enmap
     const cmd = client.commands.get(command);
   
-    // If that command doesn't exist, silently exit and do nothing
     if (!cmd) return;
   
     // Run the command
