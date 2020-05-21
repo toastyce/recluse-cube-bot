aea01f9b52e67bb4dcf3ec5cb35b834ec1dca354
toastyce <toastyce@gmail.com>
2020-05-21 15:03:44 -0700

    swap role command

--- commands/swap.js
+++ commands/swap.js
@@ -1,7 +1,17 @@
 exports.run = (client, message, args) => {
     // command starts here
     message.delete();
-    // placeholder
-    client.log.console(args);
+    var arr = [];
+    message.guild.roles.cache.forEach(role => arr.push(`${role.name}`) );
+    client.log.info(`${arr.length}`);
+    let fromMem = message.mentions.members.first();
+    let toMem = message.mentions.members.last();
+    let fromMemRole = message.mentions.members.first().roles.highest;
+    let toMemRole = message.mentions.members.last().roles.highest;
+fromMem.roles.add(`${toMemRole.id}`).catch(client.log.error).then(
+    fromMem.roles.remove(`${fromMemRole.id}`).catch(client.log.error).then(
+        toMem.roles.add(`${fromMemRole.id}`).catch(client.log.error).then(
+            toMem.roles.remove(`${toMemRole.id}`).catch(client.log.error).then(
+            ))));
     // command ends here
 }
