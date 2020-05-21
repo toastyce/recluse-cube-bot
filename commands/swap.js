23ae4c26a2b527556c77e6c0d30330ccadbdd3ee
toastyce <toastyce@gmail.com>
2020-05-21 15:24:04 -0700

    swappy finished

--- commands/swap.js
+++ commands/swap.js
@@ -1,17 +1,31 @@
 exports.run = (client, message, args) => {
     // command starts here
     message.delete();
-    var arr = [];
-    message.guild.roles.cache.forEach(role => arr.push(`${role.name}`) );
-    client.log.info(`${arr.length}`);
+
     let fromMem = message.mentions.members.first();
     let toMem = message.mentions.members.last();
     let fromMemRole = message.mentions.members.first().roles.highest;
     let toMemRole = message.mentions.members.last().roles.highest;
-fromMem.roles.add(`${toMemRole.id}`).catch(client.log.error).then(
-    fromMem.roles.remove(`${fromMemRole.id}`).catch(client.log.error).then(
-        toMem.roles.add(`${fromMemRole.id}`).catch(client.log.error).then(
-            toMem.roles.remove(`${toMemRole.id}`).catch(client.log.error).then(
-            ))));
+    fromMem.roles.add(`${toMemRole.id}`).catch(client.log.error).then(
+        fromMem.roles.remove(`${fromMemRole.id}`).catch(client.log.error).then(
+            toMem.roles.add(`${fromMemRole.id}`).catch(client.log.error).then(
+                toMem.roles.remove(`${toMemRole.id}`).catch(client.log.error).then())));
+    const embed = new client.Discord.MessageEmbed()
+        .setAuthor(`<${client.user.username}>`, client.user.avatarURL)
+        .setTitle("Swappy")
+        .setColor(client.config.colour)
+        .setDescription(client.starray.swappy.replace("{{a}}", `<@${toMem.id}>`).replace("{{b}}", `${fromMemRole.name}`).replace("{{c}}", `<@${fromMem.id}>`).replace("{{d}}", `${toMemRole.name}`))
+        .setFooter(client.starray.footer.replace("{{version}}", `${client.version}`))
+        .setTimestamp();
+    const logEmbed = new client.Discord.MessageEmbed()
+        .setAuthor(`<${client.user.username}>`, client.user.avatarURL)
+        .setTitle("Swappy")
+        .setColor(client.config.colour)
+        .setDescription(client.starray.logSwappy.replace("{{a}}", `<@${message.author.id}>`).replace("{{b}}", `<@${fromMem.id}>`).replace("{{c}}", `<@${toMem.id}>`))
+        .setFooter(client.starray.footer.replace("{{version}}", `${client.version}`))
+        .setTimestamp();
+    message.channel.send(embed).then(
+        message.guild.channels.cache.get(client.config.logChannel).send(logEmbed).catch(client.log.error)
+    )
     // command ends here
 }
