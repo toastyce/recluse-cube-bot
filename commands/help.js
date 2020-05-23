4fd786636fcee425d21cfb4abd16734b65d617c9
toastyce <toastyce@gmail.com>
2020-05-23 01:37:37 -0700

    help placeholder

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,13 @@
 exports.run = (client, message, args) => {
     // command starts here
     message.delete();
-// placeholder
+    // placeholder
+    if (!message.channel.name.startsWith('request-')) {
+        const noHelp = new client.Discord.MessageEmbed()
+            .setColor("#E74C3C")
+            .setDescription(client.starray.noHelp)
+        message.channel.send(noHelp);
+    }
     client.log.console(args);
     // command ends here
 }
