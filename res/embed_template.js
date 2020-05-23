bd732387b611ea51e7f4cb93cee6c0ce7423bb9e
toastyce <toastyce@gmail.com>
2020-05-23 01:35:55 -0700

    add/remove affinity role to channel

--- res/embed_template.js
+++ res/embed_template.js
@@ -6,3 +6,10 @@ const embed = new client.Discord.MessageEmbed()
 .setFooter(client.starray.footer.replace("{{version}}", `${client.version}`))
 .setTimestamp();
 message.channel.send(embed)
+
+if (!message.channel.name.startsWith('request-')) {
+    const notTicket = new client.Discord.MessageEmbed()
+        .setColor("#E74C3C")
+        .setDescription(client.starray.incorrectChannel)
+    return message.channel.send(notTicket);
+}
