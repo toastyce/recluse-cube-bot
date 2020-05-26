28c5498fef2686316271dc20dfa9f0537287ee94
toastyce <toastyce@gmail.com>
2020-05-26 10:02:55 -0700

    forceconfirm command

--- commands/togglespectator.js
+++ commands/togglespectator.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     //TODO: A toggle for turning on and off spectators for a room
-    
+
     // command starts here
     message.delete();
     if (!message.member.hasPermission("MANAGE_CHANNELS")) {
@@ -10,29 +10,11 @@ exports.run = (client, message, args) => {
         return message.channel.send(noPerm);
     }
 
-    if (!args[0]) {
+    var addRole = message.guild.roles.cache.get(`${client.config.memberRole}`)
 
-        client.log.debug("no affinity specified.");
-        const notAffinity = new client.Discord.MessageEmbed()
-            .setColor("#E74C3C")
-            .setDescription(client.starray.noAffinity)
-        return message.channel.send(notAffinity);
-    } else if (args[0].toLowerCase() == "light") {
-        var addRole = message.guild.roles.cache.get(`${client.config.lightRole}`)
-        client.log.debug("light affinity specified.");
-    } else if (args[0].toLowerCase() == "dark") {
-        var addRole = message.guild.roles.cache.get(`${client.config.darkRole}`)
-        client.log.debug("dark affinity specified.");
-    } else {
-        client.log.debug("We somehow ended up here.");
-        const notAffinity = new client.Discord.MessageEmbed()
-            .setColor("#E74C3C")
-            .setDescription(client.starray.noAffinity)
-        return message.channel.send(notAffinity);
-    }
     message.channel.createOverwrite(addRole, {
         VIEW_CHANNEL: true,
-        SEND_MESSAGES: true
+        SEND_MESSAGES: false
     })
 
 
