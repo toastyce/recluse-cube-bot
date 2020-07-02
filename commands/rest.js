821a859200b2f5e6c7398a67081b6394622498b5
toastyce <toastyce@gmail.com>
2020-07-01 19:46:14 -0700

    replaced confirm function

--- commands/rest.js
+++ commands/rest.js
@@ -8,14 +8,5 @@ exports.run = (client, message, args) => {
     return message.channel.send(noPerm);
   }
 
-  const embed = new client.Discord.MessageEmbed()
-    .setTitle(client.starray.fullyRest)
-    .setColor(client.config.colour)
-    .setTimestamp()
-    .addField("API Latency", `${Math.round(message.client.ws.ping)}ms`, true)
-    .setFooter(client.starray.footer.replace("{{version}}", `${client.version}`));
-  message.channel.send({
-    embed
-  })
   // command ends here
 }
